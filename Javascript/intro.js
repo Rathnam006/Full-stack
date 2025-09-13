c44889a8d6e008c87e321041841d7ffe3d402b6d
vivek rathnam <234g1a33i6@srit.ac.in>
2025-09-13 09:52:23 +0530

    javscript folder is added

---
+++ Javascript/intro.js
@@ -0,0 +1,18 @@
+console.log("hello")
+
+// age=21
+// console.log(age)
+var name="vivek"
+console.log(name)
+
+let age=20
+console.log(age )
+
+
+// var name="john"
+
+// function welcome(name){
+//     return "welcome" + name;
+// }
+//  let name=welcome()
+//  console.log()
